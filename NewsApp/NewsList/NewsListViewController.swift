3ffee73f5810463a46895d0ba140224cfe8ef3fb
M <M>
2024-03-26 17:55:01 +0300

    Подготовка проекта

---
+++ NewsApp/NewsList/NewsListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsListViewController.swift
+//  NewsApp
+//
+//  Created by Муслим on 26.03.2024.
+//
+
+import UIKit
+
+final class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
